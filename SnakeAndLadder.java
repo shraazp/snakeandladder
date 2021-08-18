57b812aa66cb4bceced20363e60a8410a62191b7
shraazp <shravyap99@gmail.com>
2021-08-18 15:21:32 +0530

    added:USeCase1

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,7 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to snake and ladder!!!!");
+		int position=1;
 	}
 
 }
