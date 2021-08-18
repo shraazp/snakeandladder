d878c0997d4dbe6aff5b349ec1a13babbcdc6c63
shraazp <shravyap99@gmail.com>
2021-08-18 15:15:40 +0530

    Welcome message

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package snake;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to snake and ladder!!!!");
+	}
+
+}
