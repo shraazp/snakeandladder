0348e347ab7c7ea4d1336f55bebe45ca44f255c3
shraazp <shravyap99@gmail.com>
2021-08-18 15:32:37 +0530

    added:USeCase2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,14 @@
 package snake;
-
+import java.util.Random;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to snake and ladder!!!!");
 		int position=1;
+		Random rand=new Random();
+		int die_number=rand.nextInt(7-1)+1;
+		System.out.println("die number:"+die_number);
 	}
 
 }
