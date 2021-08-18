bf35271408d51a1b88b49b86941c7bfbd0534d25
shraazp <shravyap99@gmail.com>
2021-08-18 16:18:58 +0530

    added:Usecase 4

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -14,21 +14,30 @@ public class SnakeAndLadder {
 		int position=1;
 		
 		Random rand=new Random();
-		
-		int die_number=rand.nextInt(7-1)+1;
-		System.out.println("die number:"+die_number);
-		
-		int option=rand.nextInt(4-1)+1;
-		switch(option)
+		while(position!=100)
 		{
-		case No_Play:System.out.println("Stays in the same position!!!");
-		break;
-		case Ladder:position+=die_number;
-		break;
-		case Snake:position-=die_number;
-		break;
+			if(position<0)
+			{
+				position=0;
+			}
+		
+			int die_number=rand.nextInt(7-1)+1;
+			System.out.println("die number:"+die_number);
+		
+			int option=rand.nextInt(4-1)+1;
+			switch(option)
+			{
+				case No_Play:System.out.println("Stays in the same position!!!");
+				break;
+				case Ladder:position+=die_number;
+							System.out.println("Ladder");
+				break;
+				case Snake:position-=die_number;
+							System.out.println("Snake");
+				break;
+			}
+			System.out.println("Position:"+position);
 		}
-		System.out.println("Position:"+position);
 	}
 
 }
